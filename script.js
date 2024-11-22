5a01e88fbaf98732866bb7ca59192d7ee19df5d9
Igor123987 <167933954+Igor123987@users.noreply.github.com>
2024-11-21 22:21:01 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 document.addEventListener("DOMContentLoaded", function() {
     const noButton = document.getElementById("noButton");
 
@@ -20,16 +21,28 @@ function showLovePage() {
     const lovePage = document.createElement("div");
     lovePage.className = "love-page";
     lovePage.innerHTML = `
-        <h1>Eu te amoooooooooo minha lua❤️❤️❤️!</h1>
-        <p>Meu amor, saber que você aceitou e está disposta a enfrentar esse desafio me deixa imensamente feliz. Eu já tinha a certeza de que você é a mulher da minha vida, e agora essa certeza só se fortaleceu ainda mais. Vou fazer o possível para estar sempre ao seu lado, meu bem. Te amo muitooooooo! Você é tudo para mim e não vejo a hora de vivermos muitos momentos felizes juntos.</p>
+        <h1>Eu te amo!</h1>
+        <p>Minha lua, eu te amo tanto que não sei explicar. Eu estava esperando o momento certo para fazer este pedido. Sei que a distância é um obstáculo, mas não é impossível. Eu quero estar ao seu lado por toda a minha vida, por isso este pedido: eu te amo!</p>
         <div class="photos">
             <!-- Adicione suas fotos aqui -->
-            <img src="fotos/foto1.jpeg" alt="Foto 1">
-            <img src="fotos/foto2.jpeg" alt="Foto 2">
-            <img src="fotos/foto3.jpeg" alt="Foto 3">
-            <img src="fotos/foto4.jpeg" alt="Foto 4">
-            <img src="fotos/foto5.jpeg" alt="Foto 5">
-            <img src="fotos/foto7.jpeg" alt="Foto 7">
+           <img src="fotos/foto1.jpeg" alt="Foto 1">
+<img src="fotos/foto2.jpeg" alt="Foto 2">
+<img src="fotos/foto3.jpeg" alt="Foto 3">
+<img src="fotos/foto4.jpeg" alt="Foto 4">
+<img src="fotos/foto5.jpeg" alt="Foto 5">
+<img src="fotos/foto7.jpeg" alt="Foto 7">
+
+        </div>
+        <div class="hearts">
+            <div class="heartbeatloader">
+                <div class="svgdraw">
+                    <svg viewBox="0 0 100 100">
+                        <path class="path" d="M10 30 Q 40 5, 70 30 T 90 30" fill="none" />
+                    </svg>
+                </div>
+                <div class="innercircle"></div>
+                <div class="outercircle"></div>
+            </div>
         </div>
     `;
     document.body.appendChild(lovePage);
